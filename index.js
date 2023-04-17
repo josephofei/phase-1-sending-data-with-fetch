33f51bc70b59a6276f0d05ff8f546d4b346bf3d5
Joseph Ofei <josephofei24@gmail.com>
2023-04-17 16:48:50 -0400

    fetch data lab complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+//str1 = username
+//str2 = email address
+
+const submitData = function (passName, passEmail) {
+
+
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: passName,
+            email: passEmail
+        }),
+
+    }).then(res => res.json())
+        .then(data => {
+            const newData = document.body.append(data.id)
+        })
+
+        //e.preventDefault;
+        .catch(function (error) {
+            const message = error.message
+            document.body.append(message)
+        })
+}
+
